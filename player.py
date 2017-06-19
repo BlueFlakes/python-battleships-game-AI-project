bb39d45963c39ffb90cc7e1507e6a080fe4bac03
JakubJaniszewski <cwanykuba@gmail.com>
2017-06-19 20:53:42 +0200

    Make Player class

---
+++ player.py
@@ -0,0 +1,26 @@
+from ocean import Ocean
+
+
+class Player:
+
+    def __init__(self):
+        self.name = ""
+        self.is_winner = False
+        self.ocean = Ocean()
+
+    def __str__(self):
+        if self.is_winner is True:
+            return "{} has won the game!".format(self.name)
+        else:
+            return "{} is a loser!".format(self.name)
+
+    def check_status(self):
+        ships_statuses = [ship.is_sunk for ship in self.ocean.ships]
+
+        self.is_winner = False if False in ships_statuses else True
+
+    def shot(self):
+        coordinates = ui.get_inputs(["First coordinate", "Second coordinate"], "Where do you want to shot?")
+        coordinates = str(coordinates[0]) + str(coordinates[1])
+
+        self.ocean.shot(coordinates)
