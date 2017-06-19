433b29d4441a5da02480aa80d2ee3ca93aff1f10
JakubJaniszewski <cwanykuba@gmail.com>
2017-06-19 21:07:53 +0200

    Make start_gameplay() function

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import os
 import ui
+from player import Player
 
 
 def main():
     option = float("inf")
-    while not option == "3":
+    while not (option == "3" or option == "1"):
         print_menu()
         option = choose()
+
+    if option == "1":
+        start_gameplay()
 
 
 def print_menu():
@@ -38,4 +42,26 @@ def choose():
     return option
 
 
-main()
+def start_gameplay():
+    """
+    Main game loop, which ends when one of the player wins.
+
+    Returns:
+        None
+    """
+
+    names = ui.get_inputs(["Player1 name", "Player2 name"], "Please provide your names")
+
+    player1 = Player(names[0])
+    player2 = Player(names[1])
+
+    while not (player1.is_winner or player2.is_winner):
+        player1.shot()
+        player2.shot()
+
+    print(player1)
+    print(player2)
+
+
+if __name__ = "__main__":
+    main()
