44ac2e3e8690d87b9f95e4b8c50b3674a3b49c3b
PawelSyktus <pawel.syktus@gmail.com>
2017-06-20 10:44:41 +0200

    Add method __str__(self)

--- ocean.py
+++ ocean.py
@@ -3,17 +3,26 @@ class Ocean:
 
     def __init__(self):
         self.ships = []
-        self.shooted_ships = []
+        self.enemy_board = []
         self.board = []
-
 
     def create_board(self):
         pass
 
+    def __str__(self):
+        board_to_display = ''
+        for row in self.board:
+            for cell in row:
+                board_to_display += str(cell)
+            board_to_display += "\n"
+        return board_to_display
+
 
 def main():
-    baltic = Ocean()
+    ocean = Ocean()
+    print(ocean.create_board())
+    print(ocean)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
