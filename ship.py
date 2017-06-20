46651fa89984885acad906ee39adb6627d048582
JakubJaniszewski <cwanykuba@gmail.com>
2017-06-20 13:19:31 +0200

    Make place_ship() method

--- ship.py
+++ ship.py
@@ -4,14 +4,15 @@ from square import Square
 
 class Ship:
 
-    def __init__(self, ship_name, column, row):
+    def __init__(self, ship_name, column, row, direction):
         self.name = ship_name.lower()
-        self.is_horizontal = False
         self.is_sunk = False
         self.start_column = int(column)
         self.start_row = int(row)
+        self.direction = direction.lower()
 
-        self.squares_list = self.set_size()
+        self.size = self.set_size()
+        self.squares = []
 
     def set_size(self):
         sizes = {"carrier": 5, "battleship": 4, "cruiser": 3, "submarine": 3, "destroyer": 2}
@@ -21,9 +22,26 @@ class Ship:
         except KeyError:
             ui.print_message("Provided ship name is wrong.")
 
-        return [Square(row, self.start_row, self.start_column) for i in range(size)]
+        return size
 
     def check_status(self):
-        is_hit_statuses = [square.is_hit for square in self.squares_list]
+        is_hit_statuses = [square.is_hit for square in self.squares]
 
         self.is_sunk = False if False in is_hit_statuses else True
+
+    def place_ship(self, board):
+
+        for i in range(self.size):
+            board[self.start_row, self.start_column].is_element_of_ship = True
+            self.squares.append(board[self.start_row, self.start_column])
+
+            if self.direction == "right":
+                self.start_column += 1
+            if self.direction == "left":
+                self.start_column -= 1
+            if self.direction == "up":
+                self.start_row -= 1
+            if self.direction == "down":
+                self.start_row += 1
+
+            return board
