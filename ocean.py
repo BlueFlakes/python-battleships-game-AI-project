785ce41739c1a9ed5a15e0fd8f278d9b883ded55
Kamil <blueflamesxcode@gmail.com>
2017-06-20 10:23:32 +0200

    file: ocean.py | added class ocean

---
+++ ocean.py
@@ -0,0 +1,19 @@
+class Ocean:
+    __board_size = 10
+
+    def __init__(self):
+        self.ships = []
+        self.shooted_ships = []
+        self.board = []
+
+
+    def create_board(self):
+        pass
+
+
+def main():
+    baltic = Ocean()
+
+
+if __name__ == "__main__":
+    main()
