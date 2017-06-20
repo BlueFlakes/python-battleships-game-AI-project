25d6a9cba018534a01fddd95d61ae6beb9e1c487
JakubJaniszewski <cwanykuba@gmail.com>
2017-06-20 11:40:25 +0200

    Update set_size() func

--- ship.py
+++ ship.py
@@ -1,27 +1,29 @@
+import ui
 from square import Square
 
 
 class Ship:
 
-    def __init__(self, ship_name):
+    def __init__(self, ship_name, column, row):
         self.name = ship_name.lower()
         self.is_horizontal = False
         self.is_sunk = False
-        self.start_column = ""
-        self.start_row = ""
+        self.start_column = int(column)
+        self.start_row = int(row)
 
         self.squares_list = self.set_size()
 
     def set_size(self):
         sizes = {"carrier": 5, "battleship": 4, "cruiser": 3, "submarine": 3, "destroyer": 2}
-        size = sizes[self.name]
 
-        return [Square() for i in range(size)]
+        try:
+            size = sizes[self.name]
+        except KeyError:
+            ui.print_message("Provided ship name is wrong.")
+
+        return [Square(row, self.start_row, self.start_column) for i in range(size)]
 
     def check_status(self):
         is_hit_statuses = [square.is_hit for square in self.squares_list]
 
         self.is_sunk = False if False in is_hit_statuses else True
-
-    def __str__(self):
-        pass
