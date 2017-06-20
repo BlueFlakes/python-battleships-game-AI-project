eec1449e4cd85ddce6abe27da9b19d7a580c5ab2
Kamil <blueflamesxcode@gmail.com>
2017-06-20 10:41:44 +0200

    File: ocean.py | added create_board method File: square.py | added correction to main func

--- ocean.py
+++ ocean.py
@@ -1,3 +1,5 @@
+from square import Square
+
 class Ocean:
     __board_size = 10
 
@@ -6,13 +8,18 @@ class Ocean:
         self.shooted_ships = []
         self.board = []
 
-
     def create_board(self):
-        pass
+        for x in range(self.__board_size):
+            temp = []
 
+            for y in range(self.__board_size):
+                temp.append(Square(x, y))
+
+            self.board.append(temp)
 
 def main():
     baltic = Ocean()
+    baltic.create_board()
 
 
 if __name__ == "__main__":
