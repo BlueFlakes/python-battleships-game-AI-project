600ab024b7b57b850a20ea6af56e037ed21f5635
PawelSyktus <pawel.syktus@gmail.com>
2017-06-21 15:22:16 +0200

    make function set_ships in module computer.py

--- models/computer.py
+++ models/computer.py
@@ -1,5 +1,5 @@
-from random import randint
-
+from random import randint, choice
+from models.ship import Ship
 from models.ocean import Ocean
 
 
@@ -19,3 +19,19 @@ class Computer:
         column = randint(0, 9)
         square = self.ocean.enemy_board[row][column]
         square.hit()
+
+    def set_ships(self):
+        directions = ['up', 'down', 'left', 'right']
+
+        for key, value in Ship.sizes.items():
+            good_statek = False
+
+            while not good_statek:
+                row = randint(0, 9)
+                column = randint(0, 9)
+                direction = choice(directions)
+                ship = Ship(key, row, column,  direction)
+                good_statek = ship.check_enviroment(self.ocean.board)
+
+            ship.place_ship(self.ocean.board)
+            self.ocean.add_ship_to_ocean(ship)
