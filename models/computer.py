d1e4b10ac593c9475b907ca770c7bf385e09876d
Marek Stopka <keramakpots@gmail.com>
2017-06-21 18:09:27 +0200

    Add last_shot field

--- models/computer.py
+++ models/computer.py
@@ -8,6 +8,7 @@ class Computer:
         self.name = "Computer"
         self.level = level
         self.ocean = Ocean()
+        self.last_shot = []
 
     def check_status(self):
         ships_statuses = [ship.is_sunk for ship in self.ocean.ships]
@@ -15,7 +16,15 @@ class Computer:
         self.is_winner = False if False in ships_statuses else True
 
     def shot(self):
-        row = randint(0, 9)
-        column = randint(0, 9)
-        square = self.ocean.enemy_board[row][column]
-        square.hit()
+        hit = False
+        while not hit:
+            row = randint(0, 9)
+            column = randint(0, 9)
+            square = self.ocean.enemy_board[row][column]
+            if self.last_shot != square:
+                if not square.is_hit:
+                    hit = True
+                    square.hit()
+                    self.last_shot = self.ocean.enemy_board[row][column]
+            elif square == self.last_shot:
+                continue
