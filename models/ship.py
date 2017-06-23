8faee8aca9a306ee0745cb3a5d4e4280ee84ea86
PawelSyktus <pawel.syktus@gmail.com>
2017-06-23 09:12:28 +0200

    Make docstrings in ship.py | PEP8

--- models/ship.py
+++ models/ship.py
@@ -4,9 +4,61 @@ from models.ocean import Ocean
 
 
 class Ship:
+    """
+    Constructs Ship object, def multiple methods for it manipulation.
+
+    sizes: dict
+        Key: ship name
+        Value: ship size
+    """
+
     sizes = {"carrier": 5, "battleship": 4, "cruiser": 3, "submarine": 3, "destroyer": 2}
 
     def __init__(self, ship_name, row, column, direction):
+        """
+        Constructs Ship object.
+
+        Parameters
+        ------------
+        ship_name: string
+        row: integer
+        column: string
+            Letters from A to J
+        direction: string
+            Choice beetwen: left, right, up, down.
+
+        __Instance Attributes__
+        ------------
+
+        * self.squares
+          - data: list
+          - description: list of square() objects
+
+        * self.name
+          - data: string
+          - description: type of ship, i. e. carrier, cruiser
+
+        * self.size
+          - data: integer
+          - description: lenght of size list
+
+        * self.direction
+          - data: string
+          - description: contains string with direction of ship: "up", "down, "left", "right"
+
+        * self.is_sunk
+          - data: boolean
+          - description: contains True if all squares elements are hit, otherwise contains False
+
+        * self.start_column
+          - data: integer
+          - description: one of coordinates which decide where ship will be placed
+
+        * self.start_row
+          - data: integer
+          - description: one of coordinates which decide where ship will be placed
+
+        """
         self.name = ship_name.lower()
         self.is_sunk = False
         self.start_column = column
@@ -17,6 +69,14 @@ class Ship:
         self.squares = []
 
     def set_size(self):
+        """
+        Sets *size* attribute based on name of ship.
+
+        Returns
+        -------
+        size: :obj:
+
+        """
 
         try:
             size = Ship.sizes[self.name]
@@ -26,6 +86,14 @@ class Ship:
         return size
 
     def check_status(self):
+        """
+        Checks if all squares of ship are hit. If yes, sets *self.is_sunk* to True.
+
+        Returns:
+        --------
+        self.is_sunk: boolean
+        """
+
         is_hit_statuses = [square.is_hit for square in self.squares]
 
         self.is_sunk = False if False in is_hit_statuses else True
@@ -33,6 +101,17 @@ class Ship:
         return self.is_sunk
 
     def place_ship(self, board):
+        """
+        Changes correct square's *is_element_of_ship* attribute to True
+
+        Parameters
+        -------
+        board: :obj:
+
+        Returns:
+        -------
+        None
+        """
         row = self.start_row
         column = self.start_column
         is_close = self.check_enviroment(board)
@@ -54,6 +133,17 @@ class Ship:
             Ui.print_message("Can\'t place a ship here!\n")
 
     def check_enviroment(self, board):
+        """
+        Returns True if there is another ship close to the new ship position, else returns False.
+
+        Parameters
+        ----------
+        board: :obj:
+
+        Returns:
+        ----------
+        is_close: boolean
+        """
 
         end_row, end_column = self.set_ship_end(board)
         if end_row == "too far" or end_column == "too far":
@@ -69,6 +159,20 @@ class Ship:
         return is_close
 
     def set_squares_around(self, board, end_row, end_column):
+        """
+        Returns list of *Square* objects which are around ship's position.
+
+        Parameters:
+        -----------
+        board: :obj:
+        end_row: int
+        end_column: int
+
+        Returns:
+        -----------
+        squares_around: list
+
+        """
 
         squares_around = []
 
@@ -90,6 +194,19 @@ class Ship:
         return squares_around
 
     def set_ship_end(self, board):
+        """
+        Returns end row and end column of ship.
+
+        Parameters:
+        -----------
+        board: :obj:
+
+        Returns:
+        -----------
+        end_row: int
+        edn_column: int
+
+        """
         end_row = self.start_row
         end_column = self.start_column
         if self.direction == "right":
