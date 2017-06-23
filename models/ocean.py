d1dd9a3d38733af58628eeaa681a329950c5df6a
PawelSyktus <pawel.syktus@gmail.com>
2017-06-23 05:38:36 +0200

    Make docstrings in models/ocean.py | PEP8

--- models/ocean.py
+++ models/ocean.py
@@ -2,17 +2,47 @@ from models.square import Square
 
 
 class Ocean:
+    """
+    Constructs a Ocean object, def multiple methods for it manipulation, returns it as a formatted string
+    """
+
     __board_size = 10
     # Creating list with alphabet, A - amount of letters for columns
     # it's used to interpret alphanumeric coordinates
     alphabet_list = [chr(i) for i in range(65, 65 + __board_size)]
 
     def __init__(self):
+        """
+        Creates Ocean object
+
+        Instance Attributes:
+        -------------------
+        * ships
+            * data: list
+            * description: list of ship objects
+
+        * board
+            * data: list
+            * description: list of nested lists, contains map for the battleship game :)
+
+        * enemy_board
+            * data: list
+            * description: list of nested lists, same as *board*
+        """
+
         self.ships = []
         self.enemy_board = [[Square() for j in range(self.__board_size)] for i in range(self.__board_size)]
         self.board = [[Square() for j in range(self.__board_size)] for i in range(self.__board_size)]
 
     def __str__(self):
+        """
+        Make string with both boards, it will be use to display boards on screen.
+
+        Returns
+        -------
+        board_to_display: string
+            String with player's *board* and *enemy_board*
+        """
         board_to_display = ''
         board, enemy_board, row_index = self.__enumerate_alpha_index()
 
@@ -28,6 +58,17 @@ class Ocean:
         return board_to_display
 
     def __enumerate_alpha_index(self):
+        """
+        Enumarating both boards upper indexes with aplha from A to J.
+
+        Returns:
+        board: list
+            List with upper indexes
+        enemy_board: list
+            List with upper indexes
+        enumerate_records: str
+        """
+
         enumerate_records = [' ' if i == 0 else str(i) for i in range(self.__board_size+2)]
         board = self.board[:]
         enemy_board = self.enemy_board[:]
@@ -37,9 +78,31 @@ class Ocean:
         return board, enemy_board, enumerate_records
 
     def add_ship_to_ocean(self, ship):
+        """
+        Add object Ship to object Ocean.
+
+        Parameters
+        ----------
+        ship: :obj:
+            Object of Ship Class.
+
+        Returns
+        -------
+        None
+        """
+
         self.ships.append(ship)
 
     def is_every_ship_sunk(self):
+        """
+        Checks if every player's ship is sunk.
+
+        Returns
+        -------
+        ships_condition: bool
+            Return True if all ships in map are sunks, else: False.
+        """
+
         ships_condition = False if False in [ship.check_status() for ship in self.ships] else True
 
         return ships_condition
