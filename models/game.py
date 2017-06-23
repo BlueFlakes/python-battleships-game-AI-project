5ec474b05d68b9a07867cee6643a936967b94cce
PawelSyktus <pawel.syktus@gmail.com>
2017-06-23 10:26:13 +0200

    Make docstrings in module game.py | PEP8

--- models/game.py
+++ models/game.py
@@ -7,7 +7,33 @@ from models.player import Player
 
 
 class Game:
+    """
+    Constructs Game object, def multiple methods for it manipulation.
+    """
     current_game = None
+    """
+    Instance Attributes
+
+    self.type
+    data: string
+        description: Contains information about type of game: singleplayer, multiplayer, simulation.
+
+    self.player1
+    data: Player object
+        description: Contains first player object
+
+    self.player2
+    data: Player object
+        description: Contains first player object
+
+    self.is_over
+    data: boolean
+        description: Contains True if one of the players wins, otherwise contains False.
+
+    self.player_in_round
+    data: Player object
+        description: Contains Player object which is currently shooting
+    """
 
     def __init__(self, player1, player2):
         self.type = "Multi player"
@@ -31,6 +57,13 @@ class Game:
         self.player_in_round = None
 
     def is_game_over(self):
+        """
+        Returns True if someone wins, otherwise returns False
+
+        Returns:
+        is_over: boolean
+
+        """
         if self.player1.check_status() is True or self.player2.check_status() is True:
             is_over = True
         else:
@@ -39,6 +72,14 @@ class Game:
         return is_over
 
     def start_game(self):
+        """
+        Calls set_ships function for both players, and draws which one will shot first.
+
+        Returns:
+        -------
+        None
+
+        """
         first_player_number = randint(1, 2)
         if first_player_number == 1:
             self.player_in_round = self.player1
@@ -53,6 +94,14 @@ class Game:
         exit = Ui.get_inputs(["Press any number to exit"], "")
 
     def set_first_player(self):
+        """
+        Set first player.
+
+        Returns:
+        -------
+        None
+        """
+
         first_player_number = randint(1, 2)
         if first_player_number == 1:
             self.player_in_round = self.player1
@@ -61,12 +110,21 @@ class Game:
             self.player_in_round = self.player2
 
     def player_switch(self):
+        """
+        Switch players
+
+        Returns:
+        -------
+        None
+        """
+
         if self.player_in_round is self.player1:
             self.player_in_round = self.player2
         else:
             self.player_in_round = self.player1
 
     def turn(self):
+
         while not self.is_over:
             Ui.print_message(self.player_in_round.name)
             Ui.print_message(self.player_in_round.ocean)
@@ -92,6 +150,20 @@ class Game:
 
     @classmethod
     def check_coordinates(cls, coordinates, alphanumeric_dict):
+        """
+        Returns True if player choose a letter between A and J, otherwise returns False.
+
+        Parameters
+        ---------
+        coordinates: list
+            list contain row and column
+        alphanumeric_dict: dict
+
+        Returns
+        --------
+        True or False
+        """
+
         if coordinates[0].isdigit():
             if coordinates[1].upper() in alphanumeric_dict and int(coordinates[1]) in range(1, 11):
                 return True
@@ -101,6 +173,14 @@ class Game:
             return False
 
     def add_player_to_highscore(self):
+        """
+        At the end of the game, if one of the player's was a Player
+        object and he won, saves data about him to .csv file.
+
+        Returns
+        --------
+        None
+        """
         if not self.player1.is_loser and isinstance(self.player1, Player):
             name = self.player1.name
             counted_shots = self.player1.counted_shots
